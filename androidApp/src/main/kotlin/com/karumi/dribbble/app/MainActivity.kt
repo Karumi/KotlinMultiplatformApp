535f1290e78b9c828ac216475ab612554bf7f3f3
Sergio Gutiérrez <Serchinastico@users.noreply.github.com>
2019-04-12 10:43:20 +0200

    Fix build (#10)
    * First attempt to make the build work in both platforms
    
    * Finish renaming the iOS app to Dribbble app
    
    * Make the Android version work
    
    * Install detekt
    
    * Add better support from the IDE and change package name
    
    * Add detekt and ktlint. Make check depend on those
    
    * Add linting to shared project
    
    * Add more gitignore values for VSCode
    
    * Add an iOS test to make the bitrise job work
    
    * Remove empty test causing swiftlint to fail
    
    * Replace shared dependency by main dependency in its name, now the iOS build works

---
+++ androidApp/src/main/kotlin/com/karumi/dribbble/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.karumi.dribbble.app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        title = "Dribbble"
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ iosApp/Dribbble app/Sources/AppDelegate.swift
@@ -0,0 +1,14 @@
+import UIKit
+import main
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    private let window: UIWindow = UIWindow()
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
