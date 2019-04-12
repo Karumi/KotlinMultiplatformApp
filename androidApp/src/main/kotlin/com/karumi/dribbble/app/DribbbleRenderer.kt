54e935f843362159b41a74e105f4620029b92436
Serchinastico <54cymru@gmail.com>
2019-04-12 12:10:30 +0200

    Configure the renderer and the layout for every single dribbble item. Show 100 fake dribbbles to see how it works

--- androidApp/src/main/kotlin/com/karumi/dribbble/app/DribbbleRenderer.kt
+++ androidApp/src/main/kotlin/com/karumi/dribbble/app/DribbbleRenderer.kt
@@ -3,24 +3,32 @@ package com.karumi.dribbble.app
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.pedrogomez.renderers.Renderer
 
 data class DribbbleItem(val id: String, val thumbnailUrl: String, val name: String, val authorName: String)
 
 class DribbbleRenderer : Renderer<DribbbleItem>() {
-  override fun inflate(inflater: LayoutInflater?, parent: ViewGroup?): View {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-  }
 
-  override fun hookListeners(rootView: View?) {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-  }
+  private lateinit var dribbbleImageView: ImageView
+  private lateinit var dribbbleNameView: TextView
+  private lateinit var dribbbleAuthorNameView: TextView
+
+  override fun inflate(inflater: LayoutInflater?, parent: ViewGroup?): View =
+          inflater?.inflate(R.layout.item_dribbble, parent, false)!!
+
+  override fun hookListeners(rootView: View?) {}
 
   override fun render() {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    dribbbleNameView.text = content.name
+    dribbbleAuthorNameView.text = context.getString(R.string.dribbbles_list_author, content.authorName)
   }
 
   override fun setUpView(rootView: View?) {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    rootView ?: return
+    dribbbleImageView = rootView.findViewById(R.id.dribbbleImage)
+    dribbbleNameView = rootView.findViewById(R.id.dribbbleName)
+    dribbbleAuthorNameView = rootView.findViewById(R.id.dribbbleAuthorName)
   }
 }
--- androidApp/src/main/kotlin/com/karumi/dribbble/app/MainActivity.kt
+++ androidApp/src/main/kotlin/com/karumi/dribbble/app/MainActivity.kt
@@ -2,11 +2,31 @@ package com.karumi.dribbble.app
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.pedrogomez.renderers.RVRendererAdapter
+import com.pedrogomez.renderers.RendererBuilder
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        title = "Dribbble"
-        setContentView(R.layout.activity_main)
-    }
+
+  private lateinit var adapter: RVRendererAdapter<DribbbleItem>
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    title = "Dribbble"
+    setContentView(R.layout.activity_main)
+    configureRecyclerView()
+  }
+
+  private fun configureRecyclerView() {
+    val builder = RendererBuilder<DribbbleItem>().bind(DribbbleItem::class.java, DribbbleRenderer())
+    adapter = RVRendererAdapter<DribbbleItem>(builder)
+    val recyclerView = findViewById<RecyclerView>(R.id.dribbbleList)
+
+    recyclerView.layoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+    recyclerView.adapter = adapter
+
+    adapter.addAll((0..100).map { (DribbbleItem("", "", "Saguaro National Park", "Nick Slater")) })
+    adapter.notifyDataSetChanged()
+  }
 }
