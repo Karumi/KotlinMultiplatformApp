5d6334c09ae98108484e38bd3e298a2258cb1f30
Serchinastico <54cymru@gmail.com>
2019-04-12 16:52:11 +0200

    Add shadow to item view

--- iosApp/Dribbble app/PhotoCollectionViewCell.swift
+++ iosApp/Dribbble app/PhotoCollectionViewCell.swift
@@ -1,11 +1,18 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageBackView: UIView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var authorName: UILabel!
 
     func configure(withItem item: PhotoListItem) {
+        imageBackView.layer.cornerRadius = 4
+        imageBackView.layer.shadowColor = UIColor.black.cgColor
+        imageBackView.layer.shadowOffset = CGSize.zero
+        imageBackView.layer.shadowRadius = 2
+        imageBackView.layer.shadowOpacity = 1
+
         name.text = item.name
         authorName.text = item.authorName
     }
